b61ad97db6d4b4ce78c265392b44e4ec93f654b4
Loeka1234 <loekalievens@hotmail.com>
2020-06-05 11:59:08 +0200

    added coronavirus

---
+++ coronavirus/precache-manifest.f40c1765e6811b359bef42cfdf6f1427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98c77e1679e96b18bd594c341da7e1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b11ac4425bf39520da5",
+    "url": "/static/css/main.3c6a2c94.chunk.css"
+  },
+  {
+    "revision": "40c2ada3811ffaa79fba",
+    "url": "/static/js/2.545bfddb.chunk.js"
+  },
+  {
+    "revision": "e9ca516d739cae43ef9beeff189e48da",
+    "url": "/static/js/2.545bfddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b11ac4425bf39520da5",
+    "url": "/static/js/main.7e2c2284.chunk.js"
+  },
+  {
+    "revision": "a3b0e90545bf5c19fc1d",
+    "url": "/static/js/runtime-main.23ec2bd3.js"
+  }
+]);
